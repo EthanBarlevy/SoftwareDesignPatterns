d65c372308be805f4cefbcaf7a0a4a4df5541081
EthanBarlevy <98473724+EthanBarlevy@users.noreply.github.com>
2024-04-02 09:23:25 -0600

    some final touches

--- MexicanRestaurant/MexicanRestaurant.cpp
+++ MexicanRestaurant/MexicanRestaurant.cpp
@@ -4,28 +4,29 @@
 int main()
 {
     // Base Dishes
-    DishComponent* dish = new Taco(5.0f, 1.0f);
-    //DishComponent* dish = new Tostada(5.0f, 1.5f);
-    //DishComponent* dish = new Burrito(5.0f, 2.0f);
+    // 
+    //DishComponent* dish = new Taco(1.0f, 1.0f);
+    DishComponent* dish = new Tostada(1.5f, 1.5f);
+    //DishComponent* dish = new Burrito(1.0f, 2.0f);
     // 
     // Meats
     // 
-    //dish = new Beef(dish);
+    dish = new Beef(dish);
     //dish = new Chicken(dish);
-    dish = new Jackfruit(dish);
+    //dish = new Jackfruit(dish);
     //
     // Addons
     //
-    //dish = new Rice(dish);
+    dish = new Rice(dish);
     //dish = new Beans(dish);
     //dish = new Cheese(dish);
-    dish = new Guacamole(dish);
+    //dish = new Guacamole(dish);
 
     MR::Vector2* Dish = dish->JudgeDish();
 
     std::cout << dish->GetDescription() << std::endl;
     std::cout << "Flavor score: " << Dish->x << "    Price: " << Dish->y << "$" << std::endl;
-    if (Dish->x <= 10)
+    if (Dish->x <= 15)
     {
         std::cout << "It tastes fine" << std::endl;
     }
